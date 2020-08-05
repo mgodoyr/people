28c341074974394cc95748658241b7f1b77f2d49
Moises David Godoy Rojas <services@gridtrue.com>
2020-08-05 15:06:15 -0400

    loopback gateway interceptor for handle global events

---
+++ api/src/interceptors/response.interceptor.ts
@@ -0,0 +1,47 @@
+import {
+  globalInterceptor,
+  Interceptor,
+  InvocationContext,
+  InvocationResult,
+  Provider,
+  ValueOrPromise,
+} from '@loopback/core';
+
+/**
+ * This class will be bound to the application as an `Interceptor` during
+ * `boot`
+ */
+@globalInterceptor('', {tags: {name: 'response'}})
+export class ResponseInterceptor implements Provider<Interceptor> {
+  /*
+  constructor() {}
+  */
+
+  /**
+   * This method is used by LoopBack context to produce an interceptor function
+   * for the binding.
+   *
+   * @returns An interceptor function
+   */
+  value() {
+    return this.intercept.bind(this);
+  }
+
+  /**
+   * The logic to intercept an invocation
+   * @param invocationCtx - Invocation context
+   * @param next - A function to invoke next interceptor or the target method
+   */
+  async intercept(
+    invocationCtx: InvocationContext,
+    next: () => ValueOrPromise<InvocationResult>,
+  ) {
+    try {
+      // Add pre-invocation logic here
+      return await next();
+    } catch (err) {
+      // Add error handling logic here
+      throw err;
+    }
+  }
+}
